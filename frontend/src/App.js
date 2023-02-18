b0e379dbe5a4a71a7617602dde36efbe7ee972ed
Sanskar Sharma <sanskarfc@gmail.com>
2023-02-18 14:31:30 +0530

    basic speed reader created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'; 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState('');
+    const [speed, setSpeed] = useState('');
+    const [words, setWords] = useState([]);
+    const [currentWord, setCurrentWord] = useState(0);
+
+    const handleTextChange = (event) => {
+        setText(event.target.value);
+    } 
+
+    const handleSpeedChange = (event) => {
+        setSpeed(event.target.value);
+    } 
+
+    useEffect(() => {
+        setWords(text.split(' ')); 
+        setCurrentWord(0);
+    }, [text]); 
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentWord(currentWord => currentWord + 1);
+        }, 1000/speed); 
+        return () => clearInterval(interval);
+    }, [speed]); 
+
+    return (
+        <div className="apppp"> 
+            <h1>{words[currentWord]}</h1>
+            <input type="text" value={text} onChange={handleTextChange} />
+            <select value={speed} onChange={handleSpeedChange}>
+                <option value="1">1 word per second</option>
+                <option value="2">2 word per second</option>
+                <option value="3">3 word per second</option>
+            </select>
+        </div>
+    );
 }
 
 export default App;
