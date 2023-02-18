d90c9c4acc1b2171b08ae718380905b07947cf2d
Sanskar Sharma <sanskarfc@gmail.com>
2023-02-19 00:22:30 +0530

    finished the entire app XD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'; 
+import React, { useState, useEffect } from 'react'; 
+import "./App.css";
 
 function App() {
-    const [text, setText] = useState('');
-    const [speed, setSpeed] = useState('');
-    const [words, setWords] = useState([]);
-    const [currentWord, setCurrentWord] = useState(0);
+  const [text, setText] = useState('');
+  const [speed, setSpeed] = useState(1);
+  const [words, setWords] = useState([]);
+  const [currentWord, setCurrentWord] = useState(0);
+  const [isReading, setIsReading] = useState(false);
 
-    const handleTextChange = (event) => {
-        setText(event.target.value);
-    } 
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+  
+  const handleSpeedChange = (event) => {
+    setSpeed(event.target.value);
+  };
+  
+  useEffect(() => {
+    setWords(text.split(' '));
+    setCurrentWord(0);
+  }, [text]);
+  
+  useEffect(() => {
+    let intervalId;
+    intervalId = setInterval(() => {
+      setCurrentWord(currentWord => currentWord + 1);
+    }, 1000 / speed * 60);
+    setCurrentWord(0);
+    return () => clearInterval(intervalId);
+  }, [isReading]);
+  
+  const handleStart = () => {
+    setIsReading(!isReading);
+    console.log("isReading: ", isReading);
+  };
 
-    const handleSpeedChange = (event) => {
-        setSpeed(event.target.value);
-    } 
-
-    useEffect(() => {
-        setWords(text.split(' ')); 
-        setCurrentWord(0);
-    }, [text]); 
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentWord(currentWord => currentWord + 1);
-        }, 1000/speed); 
-        return () => clearInterval(interval);
-    }, [speed]); 
-
-    return (
-        <div className="apppp"> 
-            <h1>{words[currentWord]}</h1>
-            <input type="text" value={text} onChange={handleTextChange} />
-            <select value={speed} onChange={handleSpeedChange}>
-                <option value="1">1 word per second</option>
-                <option value="2">2 word per second</option>
-                <option value="3">3 word per second</option>
-            </select>
-        </div>
-    );
+  return (
+    <div className='APP'>
+      <h1 className="heading"> Enter Text To Speed Read Below </h1>
+      <div className="input-field">
+          <input type="text" value={text} onChange={handleTextChange} size="40" height="100"/>
+      </div>
+      <h2 className="speedreader">Speed Read Below</h2>
+      <div className="main">
+        <div className="main-text">{words[currentWord]}</div>
+      </div> 
+      <div className="speed-control">
+          <h3>Speed Selector in WPM (words per minute)</h3><input type="text" value={speed} onChange={handleSpeedChange} size="5" />
+      </div>
+      <button onClick={handleStart}>Start</button>
+    </div>
+  );
 }
 
 export default App;
+
